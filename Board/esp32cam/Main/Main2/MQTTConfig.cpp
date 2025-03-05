e05a72664248971c27d12b0a207ee52bfbef1e1e
Vasupol Pumpech <vasupol.pu@gmail.com>
2025-03-05 22:44:00 +0700

    Update MQTTConfig.cpp

--- Board/esp32cam/Main/Main2/MQTTConfig.cpp
+++ Board/esp32cam/Main/Main2/MQTTConfig.cpp
@@ -1,10 +1,10 @@
 #include "MQTTConfig.h"
 #include "WiFiConfig.h"
 // ข้อมูล MQTT
-const char* mqtt_server = "71df9698816a4b6ea2e24e7b6e73ac93.s1.eu.hivemq.cloud";
+const char* mqtt_server = "xxx";
 const int mqtt_port = 8883;
 const char* mqtt_username = "junker";
-const char* mqtt_password = "Bb54658668";
+const char* mqtt_password = "xxx";
 
 const char* mqtt_camera = "junker/1/camera";
 const char* mqtt_SendReady = "junker/1/ready";
