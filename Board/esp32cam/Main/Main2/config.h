8e08d7ed6e7728b448c6fc5b71c93ed045f14b12
Vasupol-Pumpech <vasupol.pu@gmail.com>
2025-03-08 01:35:19 +0700

    Remove config.h from Git tracking

--- Board/esp32cam/Main/Main2/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-const char* mqtt_server = "71df9698816a4b6ea2e24e7b6e73ac93.s1.eu.hivemq.cloud";
-const int mqtt_port = 8883;
-const char* mqtt_username = "junker";
-const char* mqtt_password = "Bb54658668";
-
-#endif
